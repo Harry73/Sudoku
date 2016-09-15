e0ef3b1b4737210a06ebfe174a647ea18885d179
Ian Patel <ian.patel@yahoo.com>
2016-09-14 20:46:05 -0400

    all

---
+++ Driver.java
@@ -0,0 +1,9 @@
+// Main class to run
+public class Driver {
+	public static void main(String[] args) {
+		// Pass the command line argument to Solver, used to select debug mode
+		if (args.length == 0) {
+			Solver j = new Solver("false");
+		}
+	}
+}
