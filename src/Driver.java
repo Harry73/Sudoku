5a78fa98ddcac1dd6e39bad5d752bca38ac2f84c
Ian Patel <ian.patel@yahoo.com>
2016-09-14 21:18:06 -0400

    Remove debug option

--- src/Driver.java
+++ src/Driver.java
@@ -2,8 +2,6 @@
 public class Driver {
 	public static void main(String[] args) {
 		// Pass the command line argument to Solver, used to select debug mode
-		if (args.length == 0) {
-			Solver j = new Solver("false");
-		}
+		Solver j = new Solver("false");
 	}
 }
